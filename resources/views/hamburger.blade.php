2e0ad4c248d9fd2b89d8cf574a3555c33e0860b9
yukselkagan <yukselkagan2@gmail.com>
2021-10-12 09:28:19 +0300

    h

--- resources/views/hamburger.blade.php
+++ resources/views/hamburger.blade.php
@@ -6,7 +6,7 @@
   <div class="position-relative overflow-hidden p-3 p-md-5 m-md-0 text-center" style="background-color:green; background-image: url('image/hamburger.jpg');"  >
     <div class="col-md-5 p-lg-5 mx-auto my-5"  >
       <h1 class="display-4 fw-normal text-light">Laravel Burger</h1>
-      <p class="lead fw-normal text-light">Tasty burgers</p>
+      <p class="lead fw-normal text-light">Tasty burgers 2</p>
       <a class="btn btn-light btn-lg" href="order" style="background-color:black; color:white;" >Order</a>      
     </div>
   </div>
